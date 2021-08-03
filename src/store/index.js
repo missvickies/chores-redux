30888bd91b4bdaf370cbff13eed4d7e1c5506faf
Vicki Chen <missvickies14@gmail.com>
2021-08-03 01:22:14 -0400

    added tasks and humans

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,15 @@
-import { createStore } from 'redux';
+// import { createStore } from 'redux';
 
-export const store = createStore(
-  (state = { humans: [], tasks: [] }, action) => state
-);
+// export const store = createStore(
+//   (state = { humans: [], tasks: [] }, action) => state
+// );
+import { configureStore } from '@reduxjs/toolkit';
+import { humanSlice } from './humanSlice';
+import { taskSlice } from './taskSlice';
+
+export const store = configureStore({
+  reducer: {
+    tasks: taskSlice.reducer,
+    humans: humanSlice.reducer
+  }
+});
